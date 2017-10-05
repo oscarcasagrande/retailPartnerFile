7e4c1c1152ba7a59d88f66232e0b98da86446fc6
Oscar Casagrande <oscar.casagrande@hotmail.com.br>
2017-10-05 19:42:48 -0300

    remove old files

---
+++ retailPartnerFile/retailPartnerFile/Service/Service.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace retailPartnerFile.Service
+{
+    public static class Service
+    {
+        public static bool Importar(NameValueCollection configuracao, int codigoHistoricoExecucaoProcesso)
+        {
+            bool resultado = true;
+
+            resultado = Dominio.Parceiro.Importacao.ImportarXmlBasePorLoja(configuracao, codigoHistoricoExecucaoProcesso);
+
+            resultado = Dominio.Parceiro.Importacao.ImportarXmlImagemPorLoja(configuracao, codigoHistoricoExecucaoProcesso);
+
+            return resultado;
+        }
+
+        public static bool Transformar(NameValueCollection configuracao, int codigoHistoricoExecucaoProcesso)
+        {
+            bool resultado = false;
+
+            resultado = Dominio.Parceiro.Transformacao.TransformarXmlParceiroPorLoja(configuracao, codigoHistoricoExecucaoProcesso);
+
+            return resultado;
+        }
+
+        public static bool Exportar(NameValueCollection configuracao, int codigoHistoricoExecucaoProcesso)
+        {
+            bool resultado = false;
+
+            resultado = Dominio.Parceiro.Exportacao.ExportarXmlParceiroPorLoja(configuracao, codigoHistoricoExecucaoProcesso);
+
+            resultado = Dominio.Parceiro.Exportacao.ExportarXmlClassificacao(codigoHistoricoExecucaoProcesso);
+
+            return resultado;
+        }
+    }
+}
